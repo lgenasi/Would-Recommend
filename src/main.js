eccb086fcaf5a41aab50ce223bd106b574e34177
unknown <louisg@WKS045.ggp.local>
2015-03-10 14:06:22 +0000

    no message

--- src/main.js
+++ src/main.js
@@ -1,8 +1,3 @@
-var movies = [
-{title:"Magnolia", numYes:0, numNo:0, numOfVotes:0},
-{title:"Boogie Nights", numYes:0, numNo:0, numOfVotes:0}
-];
-
 window.onload = function(){
 	var filmDiv = document.getElementById("film");
 
