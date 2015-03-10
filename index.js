0e45f84b3fd61da9a905936b1404211fdaad0c97
HopefulLlama <jonlaw10170@gmail.com>
2015-03-10 21:09:34 +0000

    no message

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 var http = require('http');
 var express = require('express');
-var cookieParser = require('cookie-parser');
-var expressSession = require('express-session');
 
 var pkg = require('./package');
 var route = pkg.route;
 var port = pkg.port;
-var dataFile = pkg.dataFile;
 
 var exec = require('child_process').exec;
 
@@ -15,7 +12,8 @@ app.use("/src", express.static(__dirname + '/src'));
 app.use("/lib", express.static(__dirname + '/lib'));
 var server = http.createServer(app);
 
-app.set('port', (port));
+app.set('port', (process.env.PORT || 5000))
+
 app.get(route, function (req, res) {
   res.sendfile('test.html');
 });
