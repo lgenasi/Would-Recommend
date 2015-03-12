6fc5914af9d0b2e075542c5e3c28b479e94e9d03
unknown <louisg@WKS045.ggp.local>
2015-03-12 17:28:50 +0000

    Revert "pixel maniuplation"
    This reverts commit 48703844789fde8c48eb5ec87ee642f2273f27a8.

--- src/movies.js
+++ src/movies.js
@@ -1,4 +1,4 @@
 var movies = [
-{title:"Magnolia", numYes:0, numNo:0, numOfVotes:0, image:"file:///C:/dev/Would-Recommend/images/magnolia.jpg"}/*,
-{title:"Birdman", numYes:0, numNo:0, numOfVotes:0, image:"http://ia.media-imdb.com/images/M/MV5BODAzNDMxMzAxOV5BMl5BanBnXkFtZTgwMDMxMjA4MjE@._V1__SX1857_SY903_.jpg"}*/
+{title:"Magnolia", numYes:0, numNo:0, numOfVotes:0, image:"http://ia.media-imdb.com/images/M/MV5BNzMzMDgyNTIzOF5BMl5BanBnXkFtZTcwOTk0MDg2OQ@@._V1__SX1857_SY903_.jpg"},
+{title:"Birdman", numYes:0, numNo:0, numOfVotes:0, image:"http://ia.media-imdb.com/images/M/MV5BODAzNDMxMzAxOV5BMl5BanBnXkFtZTgwMDMxMjA4MjE@._V1__SX1857_SY903_.jpg"}
 ];
--- src/ui.js
+++ src/ui.js
@@ -8,12 +8,13 @@ function homePageSearch(criteria){
 
 	if(imgPath == ""){
 		buttons.style.cssText="display:none;";
-		//img.src = "http://wiki.ggpsystems.co.uk/images/Dickbutt.jpg";
+		img.src = "http://wiki.ggpsystems.co.uk/images/Dickbutt.jpg";
 	} else{
 		buttons.style.cssText="";
 		img.src = imgPath;
 	}
-}
+
+	}
 
 function bindControls(){
 	searchBtn.onclick = function(){
@@ -23,56 +24,38 @@ function bindControls(){
 	searchBox.onkeyup = function(event){
 		if (event.keyCode == 13){
 			homePageSearch(searchBox.value);
+		}
+	}
 
-			var c = document.getElementById("myCanvas");
-			var ctx = c.getContext("2d");
-			ctx.fillStyle = "blue"; 
-			ctx.fillRect(0, 0, c.width, c.height);
-			//ctx.drawImage(img, 0, 0);
-			var imgData = ctx.getImageData(0, 0, c.width, c.height);
+	var wouldRecommendBtn = document.getElementById("wouldRecommendBtn");
+	wouldRecommendBtn.onclick = function(){
+		var filmChkBox = document.getElementsByClassName("FilmCheckbox");
+		for(var i = 0; i < filmChkBox.length; i++){
+			if(filmChkBox[i].checked){
+				for(var j = 0; j <movies.length; j++){
+					if(filmChkBox[i].value == movies[j].title){
+						movies[j].numYes++;
+						movies[j].numOfVotes++;
+						window.alert(Math.round((movies[j].numYes / movies[j].numOfVotes)*100) + "% of people would recommend " + movies[j].title);
+					}
+				}
+			}
+		}
+	}
 
-			/*for (var i = 0; i < imgData.data.length; i += 4) {
-    		console.log(imgData[i  ]); // red
-    		console.log(imgData[i+1]); // green
-    		console.log(imgData[i+2]); // blue
-    		// i+3 is alpha (the fourth element)
-    	}*/
-
-		var bg = "rgb(" + imgData.data[0] + "," + imgData.data[1] + "," + imgData.data[2] + ")"
-    	document.bgColor = bg;
-    }
-
-
-    var wouldRecommendBtn = document.getElementById("wouldRecommendBtn");
-    wouldRecommendBtn.onclick = function(){
-    	var filmChkBox = document.getElementsByClassName("FilmCheckbox");
-    	for(var i = 0; i < filmChkBox.length; i++){
-    		if(filmChkBox[i].checked){
-    			for(var j = 0; j <movies.length; j++){
-    				if(filmChkBox[i].value == movies[j].title){
-    					movies[j].numYes++;
-    					movies[j].numOfVotes++;
-    					window.alert(Math.round((movies[j].numYes / movies[j].numOfVotes)*100) + "% of people would recommend " + movies[j].title);
-    				}
-    			}
-    		}
-    	}
-    }
-
-    var nahBtn = document.getElementById("nahBtn");
-    nahBtn.onclick = function(){
-    	var filmChkBox = document.getElementsByClassName("FilmCheckbox");
-    	for(var i = 0; i < filmChkBox.length; i++){
-    		if(filmChkBox[i].checked){
-    			for(var j = 0; j <movies.length; j++){
-    				if(filmChkBox[i].value == movies[j].title){
-    					movies[j].numNo++;
-    					movies[j].numOfVotes++;
-    					window.alert(Math.round((movies[j].numYes / movies[j].numOfVotes)*100) + "% of people would recommend " + movies[j].title);
-    				}
-    			}
-    		}
-    	}
-    }
-}
+	var nahBtn = document.getElementById("nahBtn");
+	nahBtn.onclick = function(){
+		var filmChkBox = document.getElementsByClassName("FilmCheckbox");
+		for(var i = 0; i < filmChkBox.length; i++){
+			if(filmChkBox[i].checked){
+				for(var j = 0; j <movies.length; j++){
+					if(filmChkBox[i].value == movies[j].title){
+						movies[j].numNo++;
+						movies[j].numOfVotes++;
+						window.alert(Math.round((movies[j].numYes / movies[j].numOfVotes)*100) + "% of people would recommend " + movies[j].title);
+					}
+				}
+			}
+		}
+	}
 }
